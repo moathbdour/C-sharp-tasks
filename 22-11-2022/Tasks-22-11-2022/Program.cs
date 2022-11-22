6380f9ecabca4f0b94532ea59283bf9a2e3ba548
Mo'ath Bdour <moathbdour@users.noreply.github.com>
2022-11-22 12:56:21 +0300

    index

---
+++ 22-11-2022/Tasks-22-11-2022/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks_22_11_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task1
+            int[] ARR = { 1, 7, 9, 45, };
+            string[] arr2 = { "Str", "alex", "moh" };
+            string[] arr3 = { "the", "fox", "over ", "lazy", "dog", };
+            //---------------------------------------------------------------------------
+
+
+
+
+            //Task2
+            //What the index of "Banana","Tomato" ?
+            //String[ ]  fruits =["Tomato", "Banana", "Watermelon"]
+
+
+            //Answers are : 0 , 1
+            //---------------------------------------------------------------------
+
+
+
+            //Task3
+            string[] FavoriteFood = { "Maftool", "maglooba", "Mlookheyah", "Kabsah", "Makmoorah" };
+            string[] FavoriteSport = { "soccer", "volleyball", "Swimming" };
+            string[] FavoriteMovies = { "Tenet", "inception", "miss peregrine's home for peculiar children" , "Harry Potter" };
+
+            //--------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+            //Task4
+
+
+            Console.Write("Enter three values: ");
+            string[] x = Console.ReadLine().Split(',');
+            int[] number = new int[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                number[i] = Convert.ToInt32(x[i]);
+            }
+            //foreach (int i in number)
+            //{
+            //    int y=y+ i;
+            //}
+            Console.WriteLine(number[0] + number[1] + number[2]);
+            //for(int i=0 ; i<number.Length; i++) { 
+
+            //    int t = t+ number[i];
+
+            //}
+
+            //--------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+            //Task5
+            int w=0;
+
+            for (int d=0 ; d < 100; d++) { 
+                if(d%2!=0)
+                {
+                    Console.Write(d+" ");
+                    w += d;
+
+                }
+            
+            
+            
+            }
+            Console.WriteLine(w);
+
+
+
+            //--------------------------------------------------------------------------------------------------------------------------------------
+
+
+            //Task6
+
+            for (int y=0 ; y < 5; y++) { 
+            
+            for(int u=0 ; u < y; u++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+
+            }
+
+            //--------------------------------------------------------------------------------------------------------------------------------------
+            //Task7
+            int c = 1;
+
+            for (int y = 0; y < 5; y++)
+            {
+                for (int r = 4; r > y; r--)
+                {
+                    Console.Write(" ");
+
+
+                }
+
+                for (int u = 0; u < y; u++)
+                {
+                  
+                    Console.Write(c + " ");
+                    c++;
+                   
+                }
+                Console.WriteLine(" ");
+
+            }
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -13,159 +13,159 @@ namespace ConsoleApp2
 
 
 
-            //Task1
+            ////Task1
 
-            Console.WriteLine("input first number");
+            //Console.WriteLine("input first number");
 
-            int firstnumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("input sec number");
+            //int firstnumber = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("input sec number");
 
-            int secnumber = Convert.ToInt32(Console.ReadLine());
-            if(firstnumber > secnumber)
-            {
-                Console.WriteLine("the biggest number is "+firstnumber);
-            }
-            else
-            {
-                Console.WriteLine("the biggest number is " + secnumber);
+            //int secnumber = Convert.ToInt32(Console.ReadLine());
+            //if(firstnumber > secnumber)
+            //{
+            //    Console.WriteLine("the biggest number is "+firstnumber);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("the biggest number is " + secnumber);
 
-            }
-
-
-            //Task2
-            Console.WriteLine("input number");
+            //}
 
 
-            int f1 = Convert.ToInt32(Console.ReadLine());
-
-            if (f1 >= 0)
-            {
-                Console.WriteLine("the sign is : +");
-            }
-            else
-            {
-                Console.WriteLine("the sign is : -");
-
-            }
+            ////Task2
+            //Console.WriteLine("input number");
 
 
-            //Task3
-            Console.WriteLine("input first number");
+            //int f1 = Convert.ToInt32(Console.ReadLine());
+
+            //if (f1 >= 0)
+            //{
+            //    Console.WriteLine("the sign is : +");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("the sign is : -");
+
+            //}
 
 
-
-            int n1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("input sec number");
-
-            int n2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("input third number");
-
-            int n3 = Convert.ToInt32(Console.ReadLine());
-
-            if (n1 > n2 && n2 > n3)
-            {
-                Console.WriteLine(n1 + " " + n2 + " " + n3);
-
-            }
-            else if (n1 > n3 && n2 < n3)
-            {
-                Console.WriteLine(n1 + " " + n3 + " " + n2);
-
-            }
-            else if (n2 > n1 && n1 > n3)
-            {
-                Console.WriteLine(n2 + " " + n1 + " " + n3);
-            }
-            else if (n2 > n3 && n1 < n3)
-            {
-                Console.WriteLine(n2 + " " + n3 + " " + n1);
-            }
-            else if (n3 > n1 && n1 > n2)
-            {
-                Console.WriteLine(n3 + " " + n1 + " " + n2);
-            }
-            else if (n3 > n2 && n1 < n2)
-            {
-                Console.WriteLine(n3 + " " + n2 + " " + n1);
-            }
-            else
-            {
-                Console.WriteLine("please dont input equel numbers and embarrass me ");
-
-            };
-
-
-            //Task4
-            Console.WriteLine("input number 1");
-
-            int m1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("input number 2");
-
-            int m2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("input number 3");
-
-            int m3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("input number 4");
-
-            int m4 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("input number 5");
-
-            int m5 = Convert.ToInt32(Console.ReadLine());
-
-            if(m1>m2&&m1>m3&& m1 > m4 && m1 > m5)
-            {
-                Console.WriteLine("the max number is : "+ m1);
-
-            }
-            else if ( m1 < m2 && m2 > m3 && m2 > m4 && m2 > m5)
-            {
-                Console.WriteLine("the max number is : " + m2);
-
-            }
-            else if (m3 > m1 && m2 < m3 && m3 > m4 && m3 > m5)
-            {
-                Console.WriteLine("the max number is : " + m3);
-
-            }
-            else if (m4 > m2 && m4 > m3 && m1 < m4 && m4 > m5)
-            {
-                Console.WriteLine("the max number is : " + m4);
-
-            }
-            else if (m5 > m2 && m5 > m3 && m5 > m4 && m1 < m5)
-            {
-                Console.WriteLine("the max number is : " + m5);
-
-            }
+            ////Task3
+            //Console.WriteLine("input first number");
 
 
 
-            //Task5
-            Console.WriteLine("Input kilometers per hour");
+            //int n1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("input sec number");
+
+            //int n2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("input third number");
+
+            //int n3 = Convert.ToInt32(Console.ReadLine());
+
+            //if (n1 > n2 && n2 > n3)
+            //{
+            //    Console.WriteLine(n1 + " " + n2 + " " + n3);
+
+            //}
+            //else if (n1 > n3 && n2 < n3)
+            //{
+            //    Console.WriteLine(n1 + " " + n3 + " " + n2);
+
+            //}
+            //else if (n2 > n1 && n1 > n3)
+            //{
+            //    Console.WriteLine(n2 + " " + n1 + " " + n3);
+            //}
+            //else if (n2 > n3 && n1 < n3)
+            //{
+            //    Console.WriteLine(n2 + " " + n3 + " " + n1);
+            //}
+            //else if (n3 > n1 && n1 > n2)
+            //{
+            //    Console.WriteLine(n3 + " " + n1 + " " + n2);
+            //}
+            //else if (n3 > n2 && n1 < n2)
+            //{
+            //    Console.WriteLine(n3 + " " + n2 + " " + n1);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("please dont input equel numbers and embarrass me ");
+
+            //};
+
+
+            ////Task4
+            //Console.WriteLine("input number 1");
+
+            //int m1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("input number 2");
+
+            //int m2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("input number 3");
+
+            //int m3 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("input number 4");
+
+            //int m4 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("input number 5");
+
+            //int m5 = Convert.ToInt32(Console.ReadLine());
+
+            //if(m1>m2&&m1>m3&& m1 > m4 && m1 > m5)
+            //{
+            //    Console.WriteLine("the max number is : "+ m1);
+
+            //}
+            //else if ( m1 < m2 && m2 > m3 && m2 > m4 && m2 > m5)
+            //{
+            //    Console.WriteLine("the max number is : " + m2);
+
+            //}
+            //else if (m3 > m1 && m2 < m3 && m3 > m4 && m3 > m5)
+            //{
+            //    Console.WriteLine("the max number is : " + m3);
+
+            //}
+            //else if (m4 > m2 && m4 > m3 && m1 < m4 && m4 > m5)
+            //{
+            //    Console.WriteLine("the max number is : " + m4);
+
+            //}
+            //else if (m5 > m2 && m5 > m3 && m5 > m4 && m1 < m5)
+            //{
+            //    Console.WriteLine("the max number is : " + m5);
+
+            //}
 
 
 
-            double ki = Convert.ToInt32(Console.ReadLine());
-            double mi = 0.621371 * ki;
-            Console.WriteLine(mi);
+            ////Task5
+            //Console.WriteLine("Input kilometers per hour");
 
 
 
-            //task 6 
+            //double ki = Convert.ToInt32(Console.ReadLine());
+            //double mi = 0.621371 * ki;
+            //Console.WriteLine(mi);
 
-            Console.WriteLine("Input hours");
 
-            int h1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Input minutes");
 
-            int i1 = Convert.ToInt32(Console.ReadLine());
+            ////task 6 
 
-            int sum = h1 * 60 + i1;
-            Console.WriteLine(sum);
+            //Console.WriteLine("Input hours");
+
+            //int h1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Input minutes");
+
+            //int i1 = Convert.ToInt32(Console.ReadLine());
+
+            //int sum = h1 * 60 + i1;
+            //Console.WriteLine(sum);
 
 
             //task 7
-            Console.WriteLine("Input hours");
+            Console.WriteLine("Input minutes");
 
 
             int o1 = Convert.ToInt32(Console.ReadLine());
